aa0e63152c5f7d8e8677e9d155312d4daefd3602
Dave King <tildedave@gmail.com>
2018-09-30 18:15:56 -0700

    Berlekamp algorithm - handle irreducibles

--- factorization_test.go
+++ factorization_test.go
@@ -18,3 +18,19 @@ func TestFactorBerlekamp(t *testing.T) {
 	assert.Contains(t, solutions, []int64{12, 4, 8, 1})
 	assert.Contains(t, solutions, []int64{6, 4, 3, 2, 1})
 }
+
+func TestFactorBerlekamp2(t *testing.T) {
+	u := make([]int64, 17)
+	u[16] = 1
+	u[1] = 1
+	fmt.Println(PolynomialToString(u))
+
+	solutions := FactorBerlekamp(u, 2)
+	fmt.Println(solutions)
+}
+
+func TestFactorBerlekampIrreducible(t *testing.T) {
+	p := []int64{1, 1, 0, 0, 1}
+	solutions := FactorBerlekamp(p, 2)
+	assert.Equal(t, 0, len(solutions))
+}
