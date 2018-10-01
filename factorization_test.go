7eb4806c256fb0799e3ae5d6411fd2019dab20ea
Dave King <tildedave@gmail.com>
2018-09-30 17:33:59 -0700

    Berkelamp Algorithm - implemented

--- factorization_test.go
+++ factorization_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var _ = fmt.Println
@@ -10,5 +12,9 @@ var _ = fmt.Println
 func TestFactorBerlekamp(t *testing.T) {
 	u := []int64{8, 2, 8, 10, 10, 0, 1, 0, 1}
 
-	FactorBerlekamp(u, 13)
+	solutions := FactorBerlekamp(u, 13)
+	assert.Equal(t, 3, len(solutions))
+	assert.Contains(t, solutions, []int64{3, 1})
+	assert.Contains(t, solutions, []int64{12, 4, 8, 1})
+	assert.Contains(t, solutions, []int64{6, 4, 3, 2, 1})
 }
