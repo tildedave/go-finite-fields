b9666a3e07997d50d8170b2edf1b5864962313ab
Dave King <tildedave@gmail.com>
2018-10-06 09:02:24 -0700

    Add test for splitting irreducibles of degree <= 6

--- factorization_test.go
+++ factorization_test.go
@@ -36,6 +36,33 @@ func TestFactorBerlekamp2(t *testing.T) {
 	// assert.Contains(t, solutions, []int64{6, 4, 3, 2, 1})
 }
 
+func countByDegree(solutions [][]int64, degree int) int {
+	numResults := 0
+	for _, solution := range solutions {
+		if PolynomialDegree(solution) == degree {
+			numResults++
+		}
+	}
+
+	return numResults
+}
+
+func TestFactorBerlekamp3(t *testing.T) {
+	u := make([]int64, 65)
+	u[64] = 1
+	u[1] = 1
+
+	// These are all the irreducibles of degree <= 6
+	solutions := FactorBerlekamp(u, 2)
+	// degree 1 (2 irreducibles) degree 2 (1), degree 3 (2) degree 6 (9)
+	assert.Equal(t, 9+2+1+2, len(solutions))
+
+	assert.Equal(t, 2, countByDegree(solutions, 1))
+	assert.Equal(t, 1, countByDegree(solutions, 2))
+	assert.Equal(t, 2, countByDegree(solutions, 3))
+	assert.Equal(t, 9, countByDegree(solutions, 6))
+}
+
 func TestFactorBerlekampIrreducible(t *testing.T) {
 	p := []int64{1, 1, 0, 0, 1}
 	solutions := FactorBerlekamp(p, 2)
